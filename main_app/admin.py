f99ef00641fbccfd68bab88c9f4d4e5c050b6dab
sagar <sagarpawar606@gmail.com>
2022-10-29 16:54:24 +0530

    Updated models.py

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,14 +1,23 @@
 from dataclasses import fields
 from django.contrib import admin
-from .models import Project, Employee, Module
+from .models import Project, Employee, Module, ModuleTeam
 # Register your models here.
 
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'status')
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'employee', 'position')
+
 class ModuleAdmin(admin.ModelAdmin):
-    list_display = ('title', 'project')
+    list_display = ('id', 'title', 'project', 'priority')
 
+class ModuleTeamAdmin(admin.ModelAdmin):
+    list_display = ('id', 'module', 'project', 'member')
 
-admin.site.register(Project)
-admin.site.register(Employee)
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Employee, EmployeeAdmin)
 admin.site.register(Module, ModuleAdmin)
+admin.site.register(ModuleTeam, ModuleTeamAdmin)
 
 
--- main_app/models.py
+++ main_app/models.py
@@ -1,10 +1,12 @@
-from random import choices
+
+from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
 project_status = [
-        ('on_track', 'On track '),
+        ('upcoming', 'Upcoming'),
+        ('active', 'Active'),
         ('postponed', 'Postponed'),
         ('delayed', 'Delayed'),
         ('complted', 'Completed'), 
@@ -26,8 +28,8 @@ class Project(models.Model):
     description = models.CharField(max_length=255, blank=True, null=True)
     start_date = models.DateField()
     deadline_date = models.DateField()
-    status = models.CharField(max_length=20, choices=project_status, default='on_track')
-    project_manager = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    status = models.CharField(max_length=20, choices=project_status, default='upcoming')
+    project_manager = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.title
@@ -42,12 +44,33 @@ class Module(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     priority = models.CharField(max_length=10, choices=module_priority, default='normal')
-    status = models.CharField(max_length=20, choices=project_status, default='in_progress')
+    status = models.CharField(max_length=20, choices=project_status, default='upcoming')
     start_date = models.DateField()
     deadline_date = models.DateField()
+    module_leader = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.title
+    
+# project_module_list = [
+#     (project.title, (
+#             (module.id, str(module.title)) for module in Module.objects.filter(project=project)
+#         ) 
+#     ) for project in Project.objects.all() 
+# ]
 
+class ModuleTeam(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    module = models.ForeignKey(Module, on_delete=models.CASCADE)
+    member = models.ForeignKey(Employee, on_delete=models.SET_NULL, null=True)
+    role = models.CharField(max_length=255)
+
+    def clean(self):
+        '''will check if combination of project and module is valid or not'''
+        if self.module.project != self.project:
+            raise ValidationError('Please enter valid module, current module doesn\'t belong to this project')
+        
+    def __str__(self):
+        return 'ModuleTeam - ' + self.module.title
 
 
